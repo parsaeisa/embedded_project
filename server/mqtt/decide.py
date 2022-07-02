81b983062d0fe5304bf2517c2192f5577ebc9d2a
AmiraliPak <a4amirali@hotmail.com>
2022-07-03 02:45:40 +0430

    add server barcode value decision

--- server/mqtt/decide.py
+++ server/mqtt/decide.py
@@ -1,3 +1,19 @@
+NOT_FOUND = '4'
+barcode_mapping = {
+    '227372240': '1',
+    '6262233100610': '2',
+    '6269109300020': '3'
+}
 
 def decide(barcode):
-    pass
+    best_match = -1
+    best_value = NOT_FOUND
+    for key, value in barcode_mapping.items():
+        if len(key) != len(barcode): continue
+        match = 0
+        for i in range(len(barcode)):
+            if barcode[i] == key[i]: match += 1
+        if match > best_match:
+            best_match = match
+            best_value = value
+    return best_value
